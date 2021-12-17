33639c379e347f366ae54ed4c5ed59fa0ee4d9d1
unknown <mohsinpeer22@gmail.com>
2021-12-18 00:45:27 +0530

    Starting Position Of Player

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    public class InitialPositionOfPlayer
+    {
+        int position = 0;
+        static void Main(String[] args)
+        {
+            InitialPositionOfPlayer c1 = new InitialPositionOfPlayer();
+            Console.WriteLine("Starting Position of Player is : " + c1.position);
+        }
+    }
+}
