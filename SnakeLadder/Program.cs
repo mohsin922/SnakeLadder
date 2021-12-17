9f003b1143478e4e59dd58835d4fd8b64eb2e655
unknown <mohsinpeer22@gmail.com>
2021-12-18 00:53:02 +0530

    Check Options using switch

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,21 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SnakeLadder
 {
-
-    public class RollTheDie
+    class InitialPositionOfPlayer
     {
+        public int position = 0;
 
-        public static int rollDie()
+    }
+
+    class Dice
+    {
+        public int dieRoll()
         {
             Random random = new Random();
             return random.Next(0, 7);
+
+
+
+        }
+    }
+    public class CheckOptions
+    {
+
+        const int NO_PLAY = 1;
+        const int LADDER = 2;
+        const int SNAKE = 3;
+        public static int check()
+        {
+            Random random = new Random();
+            return random.Next(1, 4);
         }
 
         public static void Main(String[] args)
         {
 
-            Console.WriteLine("Result of Dice Roll :  " + rollDie());
+
+            InitialPositionOfPlayer positionObj = new InitialPositionOfPlayer();
+            Dice dice = new Dice();
+
+
+            while (positionObj.position < 100)
+            {
+                int die_number = dice.dieRoll();
+
+                switch (check())
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        if (positionObj.position + die_number <= 100)
+                        {
+                            positionObj.position += die_number;
+                        }
+                        break;
+                    case SNAKE:
+                        if (positionObj.position - die_number >= 0)
+                        {
+                            positionObj.position -= die_number;
+                        }
+                        break;
+                }
+                Console.WriteLine("Current Position is : " + positionObj.position);
+            }
         }
     }
 }
